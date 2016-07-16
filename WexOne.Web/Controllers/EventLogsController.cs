0a63f9c3aa267ecf538f1efab3d40cab7347513a
Tony Qu <tony.qu@neuzilla.com>
2016-07-17 07:21:36 +0800

    Add DataTables.js to web project

---
+++ WexOne.Web/Controllers/EventLogsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WexOne.Web.Controllers
+{
+    public class EventLogsController : Controller
+    {
+        // GET: EventLogs
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
